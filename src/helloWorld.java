70445e6c0af84bc31e44c062d915fab36b13ae0a
UsaidA <85448432+UsaidA@users.noreply.github.com>
2021-10-03 15:31:19 +0100

    Initial commit

---
+++ src/helloWorld.java
@@ -0,0 +1,31 @@
+import java.awt.desktop.SystemSleepListener;
+import java.util.Scanner;
+
+public class helloWorld {
+    public static void main(String[] args) {
+        String myName = "\t\t\tUsaid Ahmed";
+        System.out.println(myName);
+
+        int integerOne= 5;
+        int integerTwo= 3;
+        int integerThree= 11;
+
+        float meanAnswer = (float) (integerOne + integerTwo + integerThree) / 3;
+        System.out.println(meanAnswer);
+
+        System.out.println("Give me a day of the week: ");
+        Scanner dayOfWeek = new Scanner(System.in);
+        String daySelected = dayOfWeek.next();
+        System.out.println("\nHappy "+dayOfWeek+"!");
+
+        System.out.println("enter a word: ");
+        Scanner userSelection = new Scanner(System.in);
+        String selectedWord = userSelection.next();
+
+
+
+
+
+    }
+
+}
